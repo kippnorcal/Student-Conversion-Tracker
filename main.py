6b437dffc8a7419889cb8fae57ab8e8a851797f6
Louis1 <louis.mendez@kippnorcal.org>
2024-03-07 10:07:53 -0800

    adding notifications

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 
+from job_notifications import create_notifications
+
 from etl_script import refresh_intervention_monitors
 from utils.school_meta_data import SCHOOL_META_DATA
 from utils.school_dataclass import create_dataclass
 
-
-
+# Create notifications object
+notifications = create_notifications("Student Conversion Tracker", "mailgun")
 
 def main():
     schools = [create_dataclass(x) for x in SCHOOL_META_DATA if x['status'] == 'ACTIVE']
@@ -17,5 +19,6 @@ def main():
 if __name__ == "__main__":
     try:
         main()
+        notifications.notify()
     except Exception as e:
         print("error running main")
